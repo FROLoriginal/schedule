37d8122e584a8ddf7e1aba7dbe12e824fadb910a
Frol <froloriginal@gmail.com>
2020-07-05 22:06:33 +0300

    Добавлены утилитные методы

--- app/src/main/java/com/example/schedule/Utils.java
+++ app/src/main/java/com/example/schedule/Utils.java
@@ -53,9 +53,9 @@ public class Utils {
     }
 
     public static String toUpperCaseFirstLetter(String input) throws NullPointerException {
-            char firstLetter = input.charAt(0);
-            input = input.substring(1);
-            return Character.toUpperCase(firstLetter) + input;
+        char firstLetter = input.charAt(0);
+        input = input.substring(1);
+        return Character.toUpperCase(firstLetter) + input;
     }
 
     public static class Time {
@@ -91,10 +91,11 @@ public class Utils {
                 } else if (current > second) {
                     return LESSON_IS_OVER;
                 } else return LESSON_WILL_START;
-            }catch (NullPointerException ex){
+            } catch (NullPointerException ex) {
                 return LESSON_IS_NOT_EXISTS;
             }
         }
+
         public static int convertUSDayOfWeekToEU(int day) {
 
             return day == 1 ? 7 : day - 1;
@@ -104,5 +105,49 @@ public class Utils {
 
             return day == 7 ? 1 : day + 1;
         }
+
+        /*
+        Timeline looks like
+
+        (12:00)x1 - - - - - - - - (13:20)x2
+                        (12:30)y1 - - - - - - - - - (14:00)y2
+         */
+        public static boolean isTimeIntersect(String t1_from, String t1_to,
+                                              String t2_from, String t2_to) {
+
+            int[] t1_from_ = stringArrToInt(t1_from.split(":"));
+            int[] t1_to_ = stringArrToInt(t1_to.split(":"));
+            int[] t2_from_ = stringArrToInt(t2_from.split(":"));
+            int[] t2_to_ = stringArrToInt(t2_to.split(":"));
+
+            int x1 = t1_from_[0] * 60 + t1_from_[1];
+            int x2 = t1_to_[0] * 60 + t1_to_[1];
+            int y1 = t2_from_[0] * 60 + t2_from_[1];
+            int y2 = t2_to_[0] * 60 + t2_to_[1];
+
+            return (x1 < y1 && y1 < x2 ||
+                    x1 < y2 && y2 < x2 ||
+                    y1 < x1 && x1 < y2 ||
+                    y1 < x2 && x2 < y2);
+
+        }
+
+        public static int timeToMinutes(String time){
+            String[] arr = time.split(":");
+            return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
+
+        }
+
+        private static int[] stringArrToInt(String[] arr) {
+
+            int[] result = new int[arr.length];
+
+            for (int i = 0; i < result.length; i++) {
+                result[i] = Integer.parseInt(arr[i]);
+            }
+            return result;
+
+        }
+
     }
 }
